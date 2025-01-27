6c39783521a0fa8763411970882d2ac36dfddbbf
nakatnan <tnap35086@gmail.com>
2025-01-27 16:44:27 +0700

    fix again

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -9,7 +9,6 @@ import Game3 from "./pages/Game3.jsx";
 import Game4 from "./pages/Game4.jsx";
 import Game5 from "./pages/Game5.jsx";
 import Game6 from "./pages/Game6.jsx";
-import Game7 from "./pages/Game7.jsx";
 
 const Router = () => {
   return (
@@ -23,7 +22,6 @@ const Router = () => {
       <Route path="/game4" element={<Game4 />} />
       <Route path="/game5" element={<Game5 />} />
       <Route path="/game6" element={<Game6 />} />
-      <Route path="/game7" element={<Game7 />} />
     </Routes>
   );
 };
